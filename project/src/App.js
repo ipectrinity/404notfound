9b4923af9a4a2ba56d8b77c9a0a3a795664f01f1
rakzool <rahulmob.3607@gmail.com>
2021-05-01 12:50:19 +0530

    preloader

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles/app.scss";
+import Preloader from "./component/Preloader/Preloader.jsx";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
     <div className="App">
       <div>
-        <h1>404 not found !!</h1>
+        <Preloader></Preloader>
       </div>
     </div>
   );
---
+++ project/src/component/Preloader/Preloader.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./preloader.scss"
+
+export default function Preloader() {
+    return (<div>
+        <div className="main-container">
+            <div class="preloader-1">
+                <span className="line line-1"></span>
+                <span className="line line-2"></span>
+                <span className="line line-3"></span>
+                <span className="line line-4"></span>
+                <span className="line line-5"></span>
+                <span className="line line-6"></span>
+                <span className="line line-7"></span>
+                <span className="line line-8"></span>
+                <span className="line line-9"></span>
+            </div>
+            <div className="image">
+                <img src="https://assets.coingecko.com/coins/images/7670/large/DogeCoin-Private-logo.png?1549240189" alt="elon-musk" className="imagin"></img>
+            </div>
+
+
+            <div className="preloader-2">
+                <span className="line line-1"></span>
+                <span className="line line-2"></span>
+                <span className="line line-3"></span>
+                <span className="line line-4"></span>
+                <span className="line line-5"></span>
+                <span className="line line-6"></span>
+                <span className="line line-7"></span>
+                <span className="line line-8"></span>
+                <span className="line line-9"></span>
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+    );
+}
